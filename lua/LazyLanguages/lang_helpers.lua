dcbf46a71fbf6e510c0286b901d8e0bd007e123e
Logan Roberts <logancroberts@outlook.com>
2024-11-13 10:22:08 -0500

    Add todo

--- lua/LazyLanguages/lang_helpers.lua
+++ lua/LazyLanguages/lang_helpers.lua
@@ -119,6 +119,7 @@ M.language_setup = function()
         utils.notify(string.format("'%s' is not a valid mason package.", package_name), { level = vim.log.levels.WARN })
       else
         local package = mason_registry.get_package(package_name)
+        -- TODO: Print error
         package:check_new_version(function(success, result_or_err)
           if success then
             if result_or_err.latest_version ~= result_or_err.current_version then
