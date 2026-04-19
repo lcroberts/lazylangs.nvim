bdcb2b2b28d932072b54e3b65439194be311f09b
Logan Roberts <logan@lcroberts.dev>
2026-04-19 10:15:08 -0400

    Update javascript language config

--- lua/lazylangs/languages/javascript.lua
+++ lua/lazylangs/languages/javascript.lua
@@ -1,27 +1,28 @@
+vim.pack.add { 'https://github.com/pmizio/typescript-tools.nvim' }
+
+require('lazylangs.helpers.lazy').by_ft(
+  { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx', 'svelte' },
+  function()
+    require('typescript-tools').setup {
+      settings = {
+        tsserver_file_preferences = {
+          includeInlayParameterNameHints = 'all',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+      },
+    }
+  end
+)
+
 ---@module "lazylangs"
 ---@type ll.Language
 local M = {
-  plugins = {
-    {
-      'pmizio/typescript-tools.nvim',
-      ft = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx', 'svelte' },
-      dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-      opts = {
-        settings = {
-          tsserver_file_preferences = {
-            includeInlayParameterNameHints = 'all',
-            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-            includeInlayFunctionParameterTypeHints = true,
-            includeInlayVariableTypeHints = true,
-            includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-            includeInlayPropertyDeclarationTypeHints = true,
-            includeInlayFunctionLikeReturnTypeHints = true,
-            includeInlayEnumMemberValueHints = true,
-          },
-        },
-      },
-    },
-  },
   mason_packages = {
     'typescript-language-server',
     'prettierd',
