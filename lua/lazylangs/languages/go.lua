d90bd76c241786b4c1a9b3ed6a6dd9daebe3be27
Logan Roberts <logan@lcroberts.dev>
2026-04-19 10:11:08 -0400

    Update go language config

--- lua/lazylangs/languages/go.lua
+++ lua/lazylangs/languages/go.lua
@@ -1,30 +1,17 @@
 ---@module "lazylangs"
+vim.pack.add {
+  'https://github.com/ray-x/go.nvim',
+  'https://github.com/ray-x/guihua.lua',
+  'https://github.com/leoluz/nvim-dap-go',
+}
+
+require('lazylangs.helpers.lazy').by_ft({ 'go', 'gomod', 'gowork', 'gotmpl' }, function()
+  require('go').setup()
+  require('dap-go').setup {}
+end)
+
 ---@type ll.Language
 return {
-  plugins = {
-    {
-      'ray-x/go.nvim',
-      dependencies = { -- optional packages
-        'ray-x/guihua.lua',
-        'neovim/nvim-lspconfig',
-        'nvim-treesitter/nvim-treesitter',
-      },
-      config = function()
-        require('go').setup()
-      end,
-      event = { 'CmdlineEnter' },
-      ft = { 'go', 'gomod', 'gowork', 'gotmpl' },
-      build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-    },
-    {
-      'leoluz/nvim-dap-go',
-      ft = { 'go', 'gomod', 'gowork', 'gotmpl' },
-      dependencies = {
-        'mfussenegger/nvim-dap',
-      },
-      opts = {},
-    },
-  },
   lsp = {
     name = 'gopls',
     server_configuration = {
