86e291d7a991eaeb6d146f3ec5577e68b72a8329
Logan Roberts <logan@lcroberts.dev>
2026-09-13 19:06:03 +0000

    Fix astro issues for if typescript is not available in project

--- lua/lazylangs/languages/astro.lua
+++ lua/lazylangs/languages/astro.lua
@@ -3,7 +3,28 @@
 local M = {
   lsp = {
     name = 'astro',
-    server_configuration = {},
+    server_configuration = {
+      before_init = function(params, config)
+        local util = require 'lspconfig.util'
+        local tsdk = util.get_typescript_server_path(config.root_dir)
+        local function has_ts(dir)
+          return dir ~= ''
+            and (vim.uv.fs_stat(vim.fs.joinpath(dir, 'typescript.js')) ~= nil
+              or vim.uv.fs_stat(vim.fs.joinpath(dir, 'tsserverlibrary.js')) ~= nil)
+        end
+
+        if not has_ts(tsdk) then
+          local mason_tsdk = vim.fs.joinpath(vim.fn.stdpath 'data', 'mason/packages/astro-language-server/node_modules/typescript/lib')
+          if has_ts(mason_tsdk) then
+            tsdk = mason_tsdk
+          end
+        end
+
+        params.initializationOptions = params.initializationOptions or {}
+        params.initializationOptions.typescript = params.initializationOptions.typescript or {}
+        params.initializationOptions.typescript.tsdk = tsdk
+      end,
+    },
   },
   mason_packages = {
     'astro-language-server',
